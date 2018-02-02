a2b063df1f363f5e6366ef17f5a0514d56b8a985
Calvin Che <calvin@cczy.io>
2018-02-02 15:45:28 +0800

    Map object with fully working set/get
    Tested with http://mdpcx3004.sce.ntu.edu.sg/mapdescriptor.php
    Web UI updated to build arena

--- web_main.py
+++ web_main.py
@@ -10,7 +10,6 @@ app.config["SECRET_KEY"] = "nHDG3Zi4HVtyc1fPBcrUEi0oACzUPRkI"
 
 @app.route('/', methods=['GET'])
 def index():
-    map = Map("BCF", 3, 4)
     return render_template('main.html')
 
 
@@ -19,24 +18,25 @@ def desc_to_array():
     part1 = request.form['part1']
     part2 = request.form['part2']
 
-    map_obj = Map(part1, part2)
+    map_obj = Map()
+    map_obj.fromMDFStrings(part1, part2)
     map_2d = [[0 for y in range(15)] for x in range(20)]
 
     for x in range(20):
         for y in range(15):
             map_2d[19-x][y] = map_obj.get(x, y)
+            
     return json.dumps(map_2d)
 
 
 @app.route('/array_to_desc', methods=['POST'])
 def array_to_desc():
     map_2d = json.loads(request.data)
-    map_obj = Map(0)
+    map_obj = Map()
     for x in range(20):
         for y in range(15):
-            if map_2d[x][y] == 1:
-                map_obj.set(19-x, y)
-    return map_obj.toHexString()
+            map_obj.set(19-x, y, map_2d[x][y])
+    return json.dumps({"part1": map_obj.toMDFPart1(), "part2": map_obj.toMDFPart2()})
 
 
 if __name__ == '__main__':
